096241ed006caa5d07fba56b0df22959c46d15bf
GGanon <77290884+gganon@users.noreply.github.com>
2021-01-24 18:58:56 +0530

    Add environment to config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
+  env: process.env.NODE_ENV || 'production',
   botToken: process.env.BOT_TOKEN,
   googleTranslationApiKey: process.env.GOOGLE_TRANSLATION_API_KEY,
 };
