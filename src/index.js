815c6659345fd256a27b3d552260a9172fa1f2a8
GGanon <77290884+gganon@users.noreply.github.com>
2021-01-24 18:58:27 +0530

    Add logger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js');
 const config = require('./config.js');
+const logger = require('./logger');
 
 const client = new Discord.Client();
 client.login(config.botToken);
+
+client.once('ready', () => {
+  logger.info('Online');
+});
---
+++ src/logger.js
@@ -0,0 +1,16 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
+const config = require('./config');
+
+const logger = createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: combine(
+    timestamp(),
+    printf(({ level, message, timestamp }) => {
+      return `[${timestamp} ${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [new transports.Console()],
+});
+
+module.exports = logger;
