b5300d03eb703d478a02c0b66a283be28a2c0aa3
GGanon <77290884+gganon@users.noreply.github.com>
2021-01-29 02:49:29 +0530

    Use translator command

--- src/message.handler.js
+++ src/message.handler.js
@@ -1,16 +1,23 @@
 const config = require('./config');
+const translate = require('./commands/translate.command');
+
+const COMMANDS = [translate];
 
 const isBotCommand = message =>
   message.content.match(
     new RegExp(`^(${config.prefix}|${config.shortPrefix})(\\s+.*)?$`)
   );
 
-const messageHandler = message => {
+const messageHandler = async message => {
   if (!isBotCommand(message) || message.author.bot) {
     return;
   }
 
-  message.channel.send('I hear ya');
+  const command = COMMANDS.find(command => command.matches(message));
+
+  if (command) {
+    await command.handler(message);
+  }
 };
 
 module.exports = messageHandler;
