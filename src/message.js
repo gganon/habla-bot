7e84e2ac273317a022497c7070575a80e7363dba
GGanon <77290884+gganon@users.noreply.github.com>
2021-01-29 02:49:15 +0530

    Implement message functions

--- src/message.js
+++ src/message.js
@@ -1,14 +1,23 @@
-const sendError = () => {
-  // TODO
+const Discord = require('discord.js');
+
+const sendError = (channel, title, details) => {
+  const message = new Discord.MessageEmbed()
+    .setTitle(title)
+    .addField('Details', details);
+  channel.send(message);
 };
 
-const sendTranslation = () => {
-  // TODO
+const sendTranslation = (channel, from, text, to, translation) => {
+  const message = `_(Translated from ${from} to ${to})_:\n\n${translation}`;
+  channel.send(message);
 };
 
-const fetchReferencedMessage = async () => {
-  // TODO
-  return { content: 'stub' }; // TODO implement
+const fetchReferencedMessage = async message => {
+  const client = message.client;
+  const { channelID, messageID } = message.reference;
+  const channel = await client.channels.fetch(channelID);
+  const referencedMessage = await channel.messages.fetch(messageID);
+  return referencedMessage;
 };
 
 module.exports = {
