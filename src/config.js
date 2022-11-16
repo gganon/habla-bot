e6f008c7379024bf2370591e66a9ba2c932ff4db
GGanon <77290884+gganon@users.noreply.github.com>
2022-11-16 15:24:38 +0530

    Handle WHITELISTED_SERVERS variable being possibly undefined

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ module.exports = {
   env: process.env.NODE_ENV || 'production',
   botToken: process.env.BOT_TOKEN,
   googleTranslationApiKey: process.env.GOOGLE_TRANSLATION_API_KEY,
-  whitelistedServers: process.env.WHITELISTED_SERVERS.split(',').filter(
-    r => !!r
-  ),
+  whitelistedServers: (process.env.WHITELISTED_SERVERS || '')
+    .split(',')
+    .filter(r => !!r),
   defaultLanguage: 'en',
   prefix: process.env.BOT_PREFIX || '!habla',
   shortPrefix: process.env.BOT_SHORT_PREFIX || '!h',
